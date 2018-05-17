fb6a2b8c9d82e6bcb18f5e17b23f41e3a4f76c5b
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-05-17 18:07:38 -0400

    completed lab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,57 @@
+require 'pry'
+require_relative './bank_account.rb'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+  return true if sender.valid? && receiver.valid? == true
+  end
+
+
+  def execute_transaction
+    if @status != "complete"
+      if sender.balance > amount && self.valid?
+         sender.balance -= amount
+         receiver.balance += amount
+        @status = "complete"
+      else
+        @status = 'rejected'
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+
+
+def reverse_transfer
+  if @status == "complete"
+    if sender.balance > amount && self.valid?
+       receiver.balance -= amount
+       sender.balance += amount
+      @status = "reversed"
+    else
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
 end
+end
+
+
+
+
+# eric = BankAccount.new("Eric",2000, 'open')
+# michael = BankAccount.new("Michael",2000, 'open')
+#
+# transfer = Transfer.new(eric, michael, 1000)
+
+# p transfer
